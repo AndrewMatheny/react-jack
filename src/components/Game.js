8c1cae7bcb4aa1e2ffef1e4d06bfa1396c4fcce5
AndrewMatheny <androidmatheny@gmail.com>
2020-05-09 21:42:03 -0700

    suits

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 let cards = ['A', 'K', 'Q', 'J', 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+let suits = ['SPADES', 'CLUBS', 'HEARTS', 'DIAMONDS']
 
 export default class Game extends React.Component {
 
